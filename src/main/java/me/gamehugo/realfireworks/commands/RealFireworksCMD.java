eaf9af5b51b33a6617fbe84f7e86a4a8f7575a79
GameHugo <85233079+GameHugo@users.noreply.github.com>
2022-09-30 20:10:37 +0200

    Added onTabComplete

--- src/main/java/me/gamehugo/realfireworks/commands/RealFireworksCMD.java
+++ src/main/java/me/gamehugo/realfireworks/commands/RealFireworksCMD.java
@@ -8,9 +8,14 @@ import me.gamehugo.realfireworks.utils.menus.FireworkMenu;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
-public class RealFireworksCMD implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RealFireworksCMD implements CommandExecutor, TabExecutor {
+    private final List<String> strings = List.of("reload");
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(!(sender instanceof Player)) {sender.sendMessage(Chat.color("&cYou can only do this as player.")); return false;}
@@ -30,5 +35,15 @@ public class RealFireworksCMD implements CommandExecutor {
         }
         FireworkMenu.open(p);
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if(!sender.hasPermission("realfireworks.use")) return new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        for (String string : strings) {
+            if(string.toLowerCase().startsWith(args[0].toLowerCase())) result.add(string);
+        }
+        return result;
     }
 }
